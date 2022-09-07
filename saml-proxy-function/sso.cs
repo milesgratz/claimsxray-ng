4fa34e12672ea425265e82d07b19d59e5613331d
Marcus Carvalho <marcusca@microsoft.com>
2022-09-07 18:19:25 +0200

    included ws-fed support

--- saml-proxy-function/sso.cs
+++ saml-proxy-function/sso.cs
@@ -17,9 +17,12 @@ namespace saml_proxy_function
             ILogger log)
         {
             var samlResponse = req.Form["samlResponse"];
-
             if (samlResponse.Count > 0)
                 return new RedirectResult($"{req.Scheme}://{req.Host}/token?samlResponse={HttpUtility.UrlEncode(samlResponse[0])}");
+
+            var wsfedResponse = req.Form["wresult"];
+            if (wsfedResponse.Count > 0)
+                return new RedirectResult($"{req.Scheme}://{req.Host}/token?wresult={HttpUtility.UrlEncode(wsfedResponse[0])}");
 
             return new RedirectResult($"{req.Scheme}://{req.Host}?error={":P"}");
         }
