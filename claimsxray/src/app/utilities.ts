42bf25c062f06a340b80383da37df7a122adfbe1
Marcus Carvalho <marcusca@microsoft.com>
2022-10-03 14:38:14 +0200

    reponse to fragment

--- claimsxray/src/app/utilities.ts
+++ claimsxray/src/app/utilities.ts
@@ -50,6 +50,24 @@ export class Utilities {
     return s;
   }
 
+  static base64UrlDecode(text: string) {
+    let encodedString = text.replace(/-/g, "+").replace(/_/g, "/");
+    switch (encodedString.length % 4) {
+        case 0:
+            break;
+        case 2:
+            encodedString += "==";
+            break;
+        case 3:
+            encodedString += "=";
+            break;
+        default:
+            throw new Error("Invalid base64 string");
+    }
+
+    return this.base64DecToArr(encodedString);
+  }
+
   static pkceCodeVerifier() {
     var verifier = new Uint8Array(32);
     for (let i = 0; i < 32; i++) {
@@ -157,5 +175,64 @@ export class Utilities {
     }
 
     return aBytes;
+  }
+
+  static utf8ArrToString (aBytes: Uint8Array): string {
+    let sView = "";
+    for (let nPart, nLen = aBytes.length, nIdx = 0; nIdx < nLen; nIdx++) {
+        nPart = aBytes[nIdx];
+        sView += String.fromCharCode(
+            nPart > 251 && nPart < 254 && nIdx + 5 < nLen ? /* six bytes */
+                /* (nPart - 252 << 30) may be not so safe in ECMAScript! So...: */
+                (nPart - 252) * 1073741824 + (aBytes[++nIdx] - 128 << 24) + (aBytes[++nIdx] - 128 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+                : nPart > 247 && nPart < 252 && nIdx + 4 < nLen ? /* five bytes */
+                    (nPart - 248 << 24) + (aBytes[++nIdx] - 128 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+                    : nPart > 239 && nPart < 248 && nIdx + 3 < nLen ? /* four bytes */
+                        (nPart - 240 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+                        : nPart > 223 && nPart < 240 && nIdx + 2 < nLen ? /* three bytes */
+                            (nPart - 224 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+                            : nPart > 191 && nPart < 224 && nIdx + 1 < nLen ? /* two bytes */
+                                (nPart - 192 << 6) + aBytes[++nIdx] - 128
+                                : /* nPart < 127 ? */ /* one byte */
+                                nPart
+        );
+    }
+    return sView;
+  }
+
+  static base64DecToArr(base64String: string, nBlockSize?: number): Uint8Array {
+      const sB64Enc = base64String.replace(/[^A-Za-z0-9\+\/]/g, "");
+      const nInLen = sB64Enc.length;
+      const nOutLen = nBlockSize ? Math.ceil((nInLen * 3 + 1 >>> 2) / nBlockSize) * nBlockSize : nInLen * 3 + 1 >>> 2;
+      const aBytes = new Uint8Array(nOutLen);
+
+      for (let nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+          nMod4 = nInIdx & 3;
+          nUint24 |= this.b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+          if (nMod4 === 3 || nInLen - nInIdx === 1) {
+              for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
+                  aBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
+              }
+              nUint24 = 0;
+          }
+      }
+
+      return aBytes;
+  }
+
+
+  static b64ToUint6(charNum: number): number {
+    return charNum > 64 && charNum < 91 ?
+        charNum - 65
+        : charNum > 96 && charNum < 123 ? 
+            charNum - 71
+            : charNum > 47 && charNum < 58 ?
+                charNum + 4
+                : charNum === 43 ?
+                    62
+                    : charNum === 47 ?
+                        63
+                        :
+                        0;
   }
 }
--- saml-proxy-function/sso.cs
+++ saml-proxy-function/sso.cs
@@ -1,10 +1,13 @@
-
-using System.Web;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
@@ -20,21 +23,38 @@ namespace saml_proxy_function
             string SAML_REDIRECT_URL = System.Environment.GetEnvironmentVariable("SAML_REDIRECT_URL");
             if (string.IsNullOrEmpty(SAML_REDIRECT_URL))
             {
-                log.LogError("The app setting SAML_REDIRECT_URL was not configured.");
-                return new StatusCodeResult(500);
+                SAML_REDIRECT_URL = System.Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME");
+                log.LogError($"The app setting SAML_REDIRECT_URL was not configured, using '{SAML_REDIRECT_URL}'.");
+                //return new StatusCodeResult(500);
             }
 
             var samlResponse = req.Form["samlResponse"];
-            if (samlResponse.Count > 0)
-                return new RedirectResult($"{SAML_REDIRECT_URL}?samlResponse={HttpUtility.UrlEncode(samlResponse[0])}");
+            if (samlResponse.Count > 0){
+                return new RedirectResult($"{SAML_REDIRECT_URL}#samlResponse={deflate(samlResponse[0])}");
+            }
 
             var wsfedResponse = req.Form["wresult"];
             if (wsfedResponse.Count > 0)
-                return new RedirectResult($"{SAML_REDIRECT_URL}?wresult={HttpUtility.UrlEncode(wsfedResponse[0])}");
+                return new RedirectResult($"{SAML_REDIRECT_URL}#wresult={deflate(wsfedResponse[0])}");
 
             log.LogWarning("The request does not have the required attributes.");
             return new BadRequestResult();
-            //return new RedirectResult($"{SAML_REDIRECT_URL}?error={":P"}");
+        }
+
+        private static string deflate(string data)
+        {
+            var bytes = Encoding.UTF8.GetBytes(data);
+
+            string deflate;
+            using (var output = new MemoryStream())
+            {
+                using (var zip = new DeflateStream(output, CompressionMode.Compress))
+                    zip.Write(bytes, 0, bytes.Length);
+
+                deflate = Base64UrlTextEncoder.Encode(output.ToArray());
+            }
+            
+            return deflate;
         }
     }
 }
